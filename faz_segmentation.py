6c2f0d1b057c775450c9b1843ee8ba6b4e2b3537
KreitnerL <linus.kreitner@tum.de>
2023-05-24 21:07:57 +0200

    perf: improve robustness of faz segmentation

--- faz_segmentation.py
+++ faz_segmentation.py
@@ -9,19 +9,19 @@ import glob
 from tqdm import tqdm
 import numpy as np
 
-source_dir = "/home/linus/Datasets/OCTA_analysis/OCTA_TUMNeuro_initial/ves_seg/Sjögren"
-data_files = natsorted(glob.glob(source_dir+'/*DVC*.png', recursive=True))
-output_dir = "/home/linus/Datasets/OCTA_analysis/OCTA_TUMNeuro_initial/faz_seg/Sjögren"
-for path in tqdm(data_files):
-    name = path.split("/")[-1]
-    if not "DVC" in name:
-        continue
+def get_faz_mask_robust(img_orig: np.ndarray) -> np.ndarray:
+    for border in [600,500,400,300,200,100]:
+        faz = get_faz_mask(img_orig, border)
+        if (faz[border+1,:]).any() or (faz[-border-1,:]).any() or (faz[:,border+1]).any() or (faz[:, -border-1]).any():
+            continue
+        return faz
 
+def get_faz_mask(img_orig: np.ndarray, BORDER=200) -> np.ndarray:
     img_orig = np.array(Image.open(path))
     img = np.copy(img_orig)
-    BORDER = 200
     img[:BORDER] = img[-BORDER:] = img[:,:BORDER]= img[:,-BORDER:] = 255
 
+    # TODO
     img_fuzzy_down = interpolate(
         torch.tensor(img[np.newaxis,np.newaxis,:,:]/255, dtype=torch.float32),
         scale_factor=.16, mode="area")
@@ -50,12 +50,30 @@ for path in tqdm(data_files):
 
     img_inverted = (1-img_merged).astype(np.float32)[np.newaxis,:,:]
     faz_final = KeepLargestConnectedComponent(connectivity=1)(img_inverted)[0].numpy()
+    return faz_final
 
 
-    # img_and_faz = np.copy(img_orig)
-    # img_and_faz[(faz_final==1) & (img_and_faz==0)]=127
-    # Image.fromarray(img_and_faz.astype(np.uint8)).save(path)
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--source_dir', help="Absolute path to the folder containing all the segmentation maps", type=str, required=True)
+    parser.add_argument('--source_files', help="Absolute path to the folder containing all the segmentation maps", type=str, default="/*")
+    parser.add_argument('--output_dir', help="Absolute path to the folder where the faz segmentation files wil be stored.", type=str, default=None)
+    args = parser.parse_args()
 
-    img_and_faz = np.zeros_like(img)
-    img_and_faz[(faz_final==1) & (img_and_faz==0)]=255
-    Image.fromarray(img_and_faz.astype(np.uint8)).save(path.replace(source_dir, output_dir).replace("pred", "faz_pred"))
+    data_files: list[str] = natsorted(glob.glob(args.source_dir + args.source_files, recursive=True))
+    output_dir: str = args.output_dir
+
+    for path in tqdm(data_files):
+        name = path.split("/")[-1]
+        img_orig = np.array(Image.open(path))
+        faz_final = get_faz_mask_robust(img_orig)
+
+        # img_and_faz = np.copy(img_orig)
+        # img_and_faz[(faz_final==1) & (img_and_faz==0)]=127
+        # Image.fromarray(img_and_faz.astype(np.uint8)).save(path)
+
+        img_and_faz = np.zeros_like(img_orig)
+        img_and_faz[(faz_final==1) & (img_and_faz==0)]=255
+        Image.fromarray(img_and_faz.astype(np.uint8)).save(path.replace(args.source_dir, output_dir).replace(name, "faz_"+name))
+
